c93d4afa2dd214ac0224b6361ffe06a26340be2d
daanniill <daniil.malakhov12@gmail.com>
2025-08-17 19:31:40 -0700

    finished styling add new book card, still work in progress though not satisfied with design

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function add_book() {
     const openButton = document.querySelector("#add_book");
     const closeButton = document.querySelector("#close_new");
-    const modal = document.querySelector(".new_book")
+    const modal = document.querySelector(".book_create")
 
     openButton.addEventListener("click", () => {
         modal.showModal();
@@ -12,4 +12,27 @@ function add_book() {
     })
 }
 
+function btn_anim() {
+    const inputs = document.querySelectorAll("input")
+    inputs.forEach(input => {
+        const label = input.previousElementSibling
+        input.addEventListener('focus', () => {
+            label.style.transform = 'translateX(0) translateY(0)'
+            label.style.fontSize = '16px';
+            input.style.color = '#040404'
+            label.style.transition = '0.2s'
+        })
+        input.addEventListener('blur', () => {
+            // Only move back if input is empty
+            if (input.value.trim() === '') {
+                label.style.transform = 'translateX(10px) translateY(28px)';
+                label.style.fontSize = '16px';
+                label.style.transition = '0.2s'
+            }
+            input.style.color = 'white'
+        })
+    })
+}
+
+btn_anim()
 add_book()
